fe7695ce6aead037e98dac56caacb1d7398352ff
Neooo17 <chpiyush17@gmail.com>
2024-12-25 23:22:49 +0530

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -132,13 +132,13 @@
 
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
 
 const App = () => {
   return (
     <>
       <Navbar />
-      <Navbar />
-      <Navbar />
+      <Footer />
     </>
       
   )
