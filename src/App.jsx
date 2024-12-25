49b1214714a553355874e4b4c88ad319a98dabbd
Neooo17 <chpiyush17@gmail.com>
2024-12-25 16:37:51 +0530

    nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -125,3 +125,25 @@
 // }
 
 // export default App
+
+
+
+
+
+import React from 'react'
+
+const App = () => {
+  return (
+      <nav>
+        <h1>Piyush</h1>
+        <div className='flex'>
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3>Service</h3>
+          <h3>Your Account</h3>
+        </div>
+      </nav>
+  )
+}
+
+export default App
