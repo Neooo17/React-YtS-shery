e6d2e87e0a287e0450f6715e34e757f29026ca5f
Neooo17 <chpiyush17@gmail.com>
2024-12-25 22:57:04 +0530

    component navbar

--- src/App.jsx
+++ src/App.jsx
@@ -131,18 +131,16 @@
 
 
 import React from 'react'
+import Navbar from './Components/Navbar'
 
 const App = () => {
   return (
-      <nav>
-        <h1>Piyush</h1>
-        <div className='flex'>
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3>Service</h3>
-          <h3>Your Account</h3>
-        </div>
-      </nav>
+    <>
+      <Navbar />
+      <Navbar />
+      <Navbar />
+    </>
+      
   )
 }
 
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <h1>Piyush</h1>
+        <div className='flex'>
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3>Service</h3>
+          <h3>Your Account</h3>
+        </div>
+      </nav>
+  )
+}
+
+export default Navbar
